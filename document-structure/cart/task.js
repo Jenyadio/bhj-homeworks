1bc67ddd5b2bbbc60e7cd1cc8fb80b999cc09600
jenyadio <jenyadio@gmail.com>
2022-01-20 23:37:03 +0300

    version 2

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,4 +1,39 @@
+const product = document.querySelectorAll('.product');
 const cartProducts = document.querySelector('.cart__products');
+
+product.forEach(elem => {
+    const dec = elem.querySelector('.product__quantity-control_dec');
+    const value = elem.querySelector('.product__quantity-value');
+    const inc = elem.querySelector('.product__quantity-control_inc');
+    const productAdd = elem.querySelector('.product__add');
+
+    dec.onclick = function() {
+        if (value.textContent > 1) {
+            value.textContent--;
+        };
+    };
+
+    inc.onclick = function() {
+        value.textContent++;
+    };
+
+    productAdd.onclick = function() {
+        if (cartProducts.querySelector(`.cart__product[data-id="${elem.dataset.id}"]`) === null) {
+            cartProducts.insertAdjacentHTML("afterbegin", `
+            <div class="cart__product" data-id="${elem.dataset.id}">
+                <img class="cart__product-image" src="${elem.querySelector('.product__image').src}">
+                <div class="cart__product-count">${value.textContent}</div>
+            </div>`);
+        } else {
+            let cartCount = cartProducts.querySelector(`.cart__product[data-id="${elem.dataset.id}"]`).querySelector('.cart__product-count');
+            cartCount.textContent = Number(cartCount.textContent) + Number(value.textContent) ;
+        };
+    };
+
+});
+
+
+/* const cartProducts = document.querySelector('.cart__products');
 
 document.addEventListener('click', (event) => {
     const inc = event.target.classList.contains('product__quantity-control_inc');
@@ -32,4 +67,4 @@ document.addEventListener('click', (event) => {
         };
     };
     event.preventDefault();
-})
+}); */
