9ce4730c79f131bb8bcf9d849d611c9175d0b1eb
jenyadio <jenyadio@gmail.com>
2022-01-26 23:12:14 +0300

    done

---
+++ async-requests/progressbar/task.js
@@ -0,0 +1,13 @@
+const progress = document.getElementById('progress');
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (event) => {
+    const formData = new FormData(form);
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php');
+    xhr.upload.addEventListener('progress', (event) => {
+            progress.value = event.loaded / event.total;
+    });
+    xhr.send(formData);
+    event.preventDefault();
+})
